bb59d3716d591b490956fbbb68b30aad273e3099
jtheis85 <jatheis2@gmail.com>
2016-03-27 22:13:37 -0700

    Browser Notification

--- bc-websocket-test/main.jsx
+++ bc-websocket-test/main.jsx
@@ -7,6 +7,16 @@ const API      = require('./API.js');
 
 var messages = [];
 
+window.addEventListener('load', () => {
+   if (window.Notification && Notification.permission !== 'granted') {
+       Notification.requestPermission((status) =>{
+          if(Notification.permission !== status) {
+              Notification.permission = status;
+          }
+       });
+   }
+});
+
 const Render = () => {
     ReactDOM.render(
         <div>
@@ -21,8 +31,12 @@ const Render = () => {
 API.connect((data) => {
     // When a message is received, update the data and re-render
     if(data.payload && data.payload.character_id && data.payload.character_id === "5428013610422131937") {
-        messages.push('Joe ' + (data.payload.event_name === 'PlayerLogin' ? 'logged in!' : 'logged out!'));
+        var message = 'Joe ' + (data.payload.event_name === 'PlayerLogin' ? 'logged in!' : 'logged out!')
+        messages.push(message);
         Render();
+        if(window.Notification && Notification.permission === 'granted') {
+            var n = new Notification(message);
+        }
     }
 }, '{"service":"event","action":"subscribe","worlds":["17"],"eventNames":["PlayerLogin","PlayerLogout"]}');
 
